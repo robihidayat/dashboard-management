b9d27ef3e31e07f8c374deed795137c9f5c9002c
robihidayat <robi.hidayat@mail.ugm.ac.id>
2018-08-13 11:42:28 +0700

    Add Project init springboot

---
+++ src/test/java/org/sekolahaluna/opensource/datamanagemenet/DataManagemenetApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sekolahaluna.opensource.datamanagemenet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataManagemenetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
